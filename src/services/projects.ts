441ac982ef204fb494c4d588bfe39feb8b96a0c3
lishay shem tov <116647050+lishayst100@users.noreply.github.com>
2025-07-06 19:51:36 +0300

    Update projects.ts
    link liron updated

--- src/services/projects.ts
+++ src/services/projects.ts
@@ -49,7 +49,7 @@ export const projects: Project[] = [
     date: "March 2023",
     desc: "An Corporate website for a real business where the details about the business are displayed and a gallery that shows the works of the business",
     git: "",
-    link: "https://shemtovilan.co.il/",
+    link: "https://lironshemtov.com/",
     framework: ["wordpress", "domain management"],
     image:
       "https://res.cloudinary.com/dabvgy03h/image/upload/v1695820174/samples/%D7%A6%D7%99%D7%9C%D7%95%D7%9D_%D7%9E%D7%A1%D7%9A_2023-09-27_115644_vejls3.png",
